68a28e492c8fb797f1531464f422185e6daf3165
Felipe Montes <frm10@psu.edu>
2026-04-29 13:26:57 -0400

    finished 2022 standards

--- GCAnalysis2022.R
+++ GCAnalysis2022.R
@@ -3628,6 +3628,338 @@ GC.standards <- GC.standards[!rownames(GC.standards) == "1381", ] ;
  })
  
  
+
+ 
+ ##############################################################################################################
+ #                           
+ #                         GC Date ["2022-11-28"]  Sampling.Date [2022-09-27]
+ #
+ ###############################################################################################################
+ 
+ 
+ 
+ ##############################################################################################################
+ #                           
+ #                         GC Date ["2022-12-05"]  Sampling.Date ["2022-10-05"]
+ #
+ ###############################################################################################################
+ 
+ str(GC.standards)
+ 
+ levels(GC.standards$ANAL.DATE)
+ 
+ unique(GC.standards$Sampling.Date)
+ 
+ Cum.Sampling.Dates
+ 
+ GC.standards[GC.standards$GC.Date == "2022-12-05",]
+ 
+ GC.standards[GC.standards$Sampling.Date == "2022-10-05",]
+ 
+ 
+ i = "2022-10-05" 
+ 
+ j = "2022_10_05"
+ 
+ ####### Plot Calibration line  for  CO2 #######
+ 
+ plot(CO2.ppm ~ CO2, 
+      
+      data = GC.standards[GC.standards$Sampling.Date == i,],
+      
+      main = i ,
+      
+      col = "blue",
+      
+      type = "p")  ;
+ 
+ text( x = GC.standards[GC.standards$Sampling.Date == i, "CO2"],
+       
+       y = GC.standards[GC.standards$Sampling.Date == i, "CO2.ppm"],
+       
+       labels = GC.standards[GC.standards$Sampling.Date == i , "Sample.Name"],
+       
+       pos = 3);
+ 
+ 
+ text( x = GC.standards[GC.standards$Sampling.Date == i, "CO2"],
+       
+       y = GC.standards[GC.standards$Sampling.Date == i, "CO2.ppm"],
+       
+       labels = rownames(GC.standards[GC.standards$Sampling.Date == i ,]),
+       
+       pos = 1);
+ 
+ 
+ 
+ ###### Calculate calibration line CO2 #######
+ 
+ assign(paste("CAL.CO2.", j, sep = ""), lm(CO2.ppm ~ CO2,
+                                           
+                                           data = GC.standards[GC.standards$Sampling.Date == i , ]))
+ 
+ 
+ summary(get(paste("CAL.CO2.", j, sep = "")))
+ 
+ get(paste("CAL.CO2.", j,sep = ""))[[1]][1]
+ 
+ abline(a = get(paste("CAL.CO2.", j,sep = ""))[[1]][1] ,
+        
+        b = get(paste("CAL.CO2.", j,sep = ""))[[1]][2], 
+        
+        col = "red") ;
+ 
+ text( x = mean(GC.standards[GC.standards$Sampling.Date == i , c("CO2")]),
+       
+       y = mean(GC.standards[GC.standards$Sampling.Date == i , c("CO2.ppm")]),
+       
+       labels = paste(round(get(paste("CAL.CO2.", j,sep = ""))[[1]][1],3), 
+                      
+                      round(get(paste("CAL.CO2.", j,sep = ""))[[1]][2],3), 
+                      
+                      sep = "+")
+ )
+ 
+ 
+ 
+ ####### Plot Calibration line  for N2O #######
+ 
+ plot(N2O.ppm ~ N2O, 
+      
+      data = GC.standards[GC.standards$Sampling.Date == i,],
+      
+      main = i,
+      
+      col = "blue",
+      
+      type = "p")  ;
+ 
+ text( x = GC.standards[GC.standards$Sampling.Date == i , "N2O"],
+       
+       y = GC.standards[GC.standards$Sampling.Date == i , "N2O.ppm"],
+       
+       labels = GC.standards[GC.standards$Sampling.Date == i , "Sample.Name"],
+       
+       pos = 3);
+ 
+ text( x = GC.standards[GC.standards$Sampling.Date == i, "N2O"],
+       
+       y = GC.standards[GC.standards$Sampling.Date == i, "N2O.ppm"],
+       
+       labels = rownames(GC.standards[GC.standards$Sampling.Date == i ,]),
+       
+       pos = 1);
+ 
+ ###### Calculate calibration line N2O #######
+ 
+ assign(paste("CAL.N2O.", j, sep = ""), lm(N2O.ppm ~ N2O,
+                                           
+                                           data = GC.standards[GC.standards$Sampling.Date == i,]))
+ 
+ 
+ 
+ summary(get(paste("CAL.N2O.", j, sep = "")))
+ 
+ abline(a = get(paste("CAL.N2O.", j, sep = ""))[[1]][1] ,
+        
+        b = get(paste("CAL.N2O.", j, sep = ""))[[1]][2], 
+        
+        col = "red") ;
+ 
+ text( x = mean(GC.standards[GC.standards$Sampling.Date == i , c("N2O")]),
+       
+       y = mean(GC.standards[GC.standards$Sampling.Date == i , c("N2O.ppm")]),
+       
+       labels = paste(round(get(paste("CAL.N2O.", j, sep = ""))[[1]][1],3), round(get(paste("CAL.N2O.", j, sep = ""))[[1]][2],3), sep = "+")
+       
+ )
+ 
+ 
+ 
+ 
+ # points(N2O.ppm ~ N2O, 
+ #        
+ #        data = GC.standards[rownames(GC.standards) == "716", ],
+ #        
+ #        col = "red",
+ #        
+ #        cex = 2,
+ #        
+ #        pch = 19) ;
+ # 
+ 
+ ##### Remove outliers row names 2326 , 2512 , 2406 , 2430  ####### 
+ 
+  GC.standards[rownames(GC.standards) == "2326", ]
+ 
+  GC.standards <- GC.standards[!rownames(GC.standards) == "2326", ] ;
+  
+  GC.standards[rownames(GC.standards) == "2512", ]
+  
+  GC.standards <- GC.standards[!rownames(GC.standards) == "2512", ] ;
+  
+  GC.standards[rownames(GC.standards) == "2406", ]
+  
+  GC.standards <- GC.standards[!rownames(GC.standards) == "2406", ] ; 
+  
+  GC.standards[rownames(GC.standards) == "2430", ]
+  
+  GC.standards <- GC.standards[!rownames(GC.standards) == "2430", ] ; 
+ 
+ ##############################################################################################################
+ #                           
+ #                           Cumulative plot for 2022
+ #
+ ###############################################################################################################
+ 
+ ##### data to plot  CO2 #####
+ 
+ i ; j ;
+ 
+ 
+ str(GC.standards$Sampling.Date)
+ 
+ Cum.Sampling.Dates <- c("2022-06-30",
+                         
+                         "2022-06-09", 
+                         
+                         "2022-06-23", 
+                         
+                         "2022-06-29", 
+                         
+                         "2022-06-15", 
+                         
+                         "2022-07-07",
+                         
+                         "2022-07-19",
+                         
+                         "2022-09-01",
+                         
+                         "2022-09-10",
+                         
+                         "2022-09-27",
+                         
+                         "2022-10-05")
+ 
+ 
+ Cum.Plot.Data <-  GC.standards[which(GC.standards$Sampling.Date %in% Cum.Sampling.Dates),] ;
+ 
+ str(Cum.Plot.Data)
+ 
+ Cum.Plot.CO2 <- xyplot(CO2 ~ CO2.ppm, 
+                        
+                        groups = GC.Date, 
+                        
+                        data = Cum.Plot.Data, 
+                        
+                        pch =16, 
+                        
+                        main="CO2", 
+                        
+                        auto.key = T, 
+                        
+                        panel=function(x, y, ...){
+                          
+                          panel.xyplot(x, y, ...)
+                          
+                          panel.text(x=x, y=y, 
+                                     
+                                     labels= rownames(Cum.Plot.Data), 
+                                     
+                                     pos=1)
+                          
+                        })
+ 
+ Cum.Plot.CO2
+ 
+ ## insert additional points
+ 
+ update(Cum.Plot.CO2, panel = function(...) {
+   
+   panel.xyplot(...)
+   
+   panel.xyplot(x = Cum.Plot.Data[Cum.Plot.Data$Sampling.Date == i, "CO2.ppm" ],
+                
+                y = Cum.Plot.Data[Cum.Plot.Data$Sampling.Date == i, "CO2" ], 
+                
+                
+                pch = 19,
+                
+                col = "red",
+                
+                cex = 1.5
+                
+   )
+   
+   panel.text(x = Cum.Plot.Data[Cum.Plot.Data$Sampling.Date == i , "CO2.ppm" ],
+              
+              y = Cum.Plot.Data[Cum.Plot.Data$Sampling.Date == i , "CO2" ], 
+              
+              labels= rownames(Cum.Plot.Data[Cum.Plot.Data$Sampling.Date == i ,]), 
+              
+              pos=1)
+ })
+ 
+ ##### data to plot  N20 #####
+ 
+ str(GC.standards$Sampling.Date)
+ 
+ 
+ str(Cum.Plot.Data)
+ 
+ Cum.Plot.N2O <- xyplot(N2O ~ N2O.ppm, 
+                        
+                        groups = GC.Date, 
+                        
+                        data = Cum.Plot.Data, 
+                        
+                        pch =16, 
+                        
+                        main = "N2O", 
+                        
+                        auto.key = T, 
+                        
+                        panel=function(x, y, ...){
+                          
+                          panel.xyplot(x, y, ...)
+                          
+                          panel.text(x=x, y=y, 
+                                     
+                                     labels= rownames(Cum.Plot.Data), 
+                                     
+                                     pos=1)
+                          
+                        }
+                        
+ )
+ 
+ Cum.Plot.N2O
+ 
+ update(Cum.Plot.N2O, panel = function(...) {
+   
+   panel.xyplot(...)
+   
+   panel.xyplot(x = Cum.Plot.Data[Cum.Plot.Data$Sampling.Date == i , "N2O.ppm" ],
+                
+                y = Cum.Plot.Data[Cum.Plot.Data$Sampling.Date == i , "N2O" ], 
+                
+                
+                pch = 19,
+                
+                col = "red",
+                
+                cex = 1.5
+                
+   )
+   
+   panel.text(x = Cum.Plot.Data[Cum.Plot.Data$Sampling.Date == i , "N2O.ppm" ],
+              
+              y = Cum.Plot.Data[Cum.Plot.Data$Sampling.Date == i , "N2O" ], 
+              
+              labels= rownames(Cum.Plot.Data[Cum.Plot.Data$Sampling.Date == i ,]), 
+              
+              pos=1)
+ })
  
  
  
